5611ab3cb881192fea327479d260794b6840fab8
VanIL <139588450+VaniLb@users.noreply.github.com>
2024-04-26 17:09:57 +0300

    Add files via upload

--- build/main.py
+++ build/main.py
@@ -144,6 +144,115 @@ async def process_start_command(message: types.Message):
         con.close()
 
 
+@dp.message_handler(commands=['other_functions'])
+async def process_start_command(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.full_name
+    try:
+        await message.answer(dict_ru['msg_other_functions'], reply_markup=other_func_menu)
+    except Exception as ex:
+        print(ex)
+
+
+@dp.message_handler(commands=['help'])
+async def process_start_command(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.full_name
+    try:
+        await message.answer(dict_ru['msg_help'])
+    except Exception as ex:
+        print(ex)
+
+
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('leson_morse'))
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    user_name = callback_query.from_user.full_name
+    try:
+        code = callback_query.data[-1]
+        if code.isdigit():
+            code = int(code)
+        if code == 0:
+            await bot.send_message(user_id, dict_ru['msg_leson_morse_1'], reply_markup=leson_morse_1_menu)
+        elif code == 1:
+            await bot.send_message(user_id, dict_ru['msg_leson_morse_2'], reply_markup=leson_morse_2_menu)
+        elif code == 2:
+            await bot.send_message(user_id, dict_ru['msg_leson_morse_3'], reply_markup=leson_morse_3_menu)
+        elif code == 3:
+            await bot.send_message(user_id, dict_ru['msg_sub'], reply_markup=sub_menu)
+    except Exception as ex:
+        print(ex)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'help_me')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    user_name = callback_query.from_user.full_name
+    try:
+        await bot.send_message(user_id, 'С любой проблемой обращаться к @Vanilb_ka')
+    except Exception as ex:
+        print(ex)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'table_trans')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    user_name = callback_query.from_user.full_name
+    try:
+        photo = open('photo/morse_photo.jpg', 'rb')
+        await bot.send_message(user_id, dict_ru['msg_table_trans'])
+        await bot.send_photo(user_id, photo)
+    except Exception as ex:
+        print(ex)
+
+
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('check_self'))
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    user_name = callback_query.from_user.full_name
+    try:
+        code = callback_query.data[-1]
+        if code.isdigit():
+            code = int(code)
+        else:
+            await bot.send_message(user_id, dict_ru['msg_check_self'], reply_markup=check_self_1)
+        if code == 1:
+            await bot.send_poll(user_id, 'Расшифровка: • — • •  • • — —  — • • •  — — —  • — —  — • • —',
+                                ['Счастье', 'Любовь', 'Разум', 'Точка-тире'],
+                                type='quiz', correct_option_id=1, is_anonymous=True, reply_markup=check_self_2)
+        elif code == 2:
+            await bot.send_poll(user_id, 'Средство для общения по Азбуке Морзе',
+                                ['Маятник', 'Телескоп', 'Телеграф'],
+                                type='quiz', correct_option_id=2, is_anonymous=True, reply_markup=check_self_3)
+        elif code == 3:
+            await bot.send_poll(user_id, 'Как на Азбуке Морзе будет СОС',
+                                ['• • •  — — —  • • •', '• • •  — — —  • • •  — — —',
+                                    '• • •  • •  • • •', '• • •  — — —  • • •  • —  —  — • • —'],
+                                type='quiz', correct_option_id=0, is_anonymous=True, reply_markup=check_self_4)
+        elif code == 4:
+            await bot.send_poll(user_id, 'Расшифровка: • — • •  • • — —',
+                                ['Ром', 'Кар', 'Блум', 'Лю'],
+                                type='quiz', correct_option_id=3, is_anonymous=True, reply_markup=check_self_5)
+        elif code == 5:
+            await bot.send_poll(user_id, 'Нужна ли Азбука Морзе сейчас? (2024 год)',
+                                ['Очень нужна', 'Сомневаюсь', 'Нет.'],
+                                type='quiz', correct_option_id=2, is_anonymous=True, reply_markup=check_self_finish)
+        elif code == 6:
+            await bot.send_message(user_id, 'Спасибо за участие, ты молодец в любом случае!')
+    except Exception as ex:
+        print(ex)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'sub')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    user_name = callback_query.from_user.full_name
+    try:
+        await bot.send_message(user_id, dict_ru['msg_sub_full'], reply_markup=sub_full_menu)
+    except Exception as ex:
+        print(ex)
+
+
 @dp.message_handler()
 async def list_status(message: types.Message):
     user_id = message.from_user.id
--- build/menu.py
+++ build/menu.py
@@ -1,1 +1,46 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+other_func_menu_btn = [
+    [InlineKeyboardButton(text="📝 Обучение Азбуке Морзе",
+                          callback_data="leson_morse_0")],
+    [InlineKeyboardButton(text="🔎 Таблица переводов", callback_data="table_trans"),
+     InlineKeyboardButton(text="✅ Проверь себя!", callback_data="check_self")],
+    [InlineKeyboardButton(text="⁉️ Техническая поддержка", callback_data="help_me")]]
+
+other_func_menu = InlineKeyboardMarkup(inline_keyboard=other_func_menu_btn)
+
+leson_morse_1_menu = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="📝 Получить 2 Урок",
+                                                                                 callback_data="leson_morse_1")]])
+
+leson_morse_2_menu = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="📝 Получить 3 Урок",
+                                                                                 callback_data="leson_morse_2")]])
+
+leson_morse_3_menu = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="📝 Получить 4 урок",
+                                                                                 callback_data="leson_morse_3")]])
+
+sub_menu = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="💎 Оформить подписку",
+                                                                       callback_data="sub")]])
+
+sub_full_menu = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="💎 Premium плейлист",
+                                                                            url="https://ru.wikipedia.org/wiki/Облом")]])
+
+check_self_1 = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="❓ 1 Вопрос",
+                                                                           callback_data="check_self_1")]])
+
+check_self_2 = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="❓ 2 Вопрос",
+                                                                           callback_data="check_self_2")]])
+
+check_self_3 = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="❓ 3 Вопрос",
+                                                                           callback_data="check_self_3")]])
+
+check_self_4 = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="❓ 4 Вопрос",
+                                                                           callback_data="check_self_4")]])
+
+check_self_5 = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="❓ 5 Вопрос",
+                                                                           callback_data="check_self_5")]])
+
+check_self_finish = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="❓ Результаты",
+                                                                                callback_data="check_self_6")]])
+exit_kb = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
+iexit_kb = InlineKeyboardMarkup(inline_keyboard=[
+                                [InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
